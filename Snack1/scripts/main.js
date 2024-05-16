22f6a82c16af4ca1f8d592e2904474c2423615e4
MykolaDekusar <ndekusar97@gmail.com>
2024-05-16 17:18:20 +0200

    Snack1 Completed

--- Snack1/scripts/main.js
+++ Snack1/scripts/main.js
@@ -1,1 +1,39 @@
 'use strict';
+
+const biciCorsa = [
+    {
+        nome: 'BMX',
+        peso: '10kg'
+    },
+    {
+        nome: 'Sport',
+        peso: '2kg'
+    },
+    {
+        nome: 'Mountain Bike',
+        peso: '5kg'
+    },
+    {
+        nome: 'Classic',
+        peso: '12kg'
+    },
+    {
+        nome: 'Electric',
+        peso: '25kg'
+    },
+]
+//initialising weight to first bike
+let minWeight = parseInt(biciCorsa[0].peso);
+let counter = 0;
+//comparing the first weight to each object of the array
+biciCorsa.forEach((element) => {
+    const weight = (parseInt(element.peso));
+    if (minWeight > weight) {
+        counter++;
+        minWeight = weight;
+    }
+    console.log(minWeight);
+});
+
+console.log(`The bike with the less weight is: ${biciCorsa[counter].nome}, with the weight of: ${minWeight}KG`);
+alert(`The bike with the less weight is: ${biciCorsa[counter].nome}, with the weight of: ${minWeight}KG`);
